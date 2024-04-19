93224f6d123779136484843447636b83034d63c8
SoyManuelPerez <Soy_ManuelPerez@outlook.com>
2024-04-18 20:10:54 -0500

    modelos y rutas de habiaciones

--- controllers/bar_controllers.js
+++ controllers/bar_controllers.js
@@ -1,6 +1,17 @@
 const Bar = require('../models/Bar')
 const HB = require('../models/HB-1')
 const HB2 = require('../models/HB-2')
+const HB3 = require('../models/HB-3')
+const HB4 = require('../models/HB-4')
+const HB5 = require('../models/HB-5')
+const HB6 = require('../models/HB-6')
+const HB7 = require('../models/HB-7')
+const HB8 = require('../models/HB-8')
+const HB9 = require('../models/HB-9')
+const HB10 = require('../models/HB-10')
+const HB11 = require('../models/HB-11')
+const HB12 = require('../models/HB-12')
+const HB13 = require('../models/HB-13')
 //Mostrar productos
 module.exports.mostrar = (req, res) => {
     Bar.find({})
@@ -44,3 +55,202 @@ module.exports.HB2 = async (req, res) => {
         res.status(500).send("Error interno del servidor");
     }
 };
+module.exports.HB3 = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const producto = await Bar.findById(id).lean().exec();
+        const Producto = producto.Producto;
+        const Precio = producto.Precio;
+        const Tipo = producto.Tipo;
+        const Usuario = "Manuel";
+        const Hora =producto.Hora
+        const newUsuario = new HB3({ Producto, Precio, Usuario, Tipo, Hora });
+        await newUsuario.save();
+        await Bar.findByIdAndDelete(id).lean().exec();
+        res.redirect('/Bar');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error interno del servidor");
+    }
+};
+module.exports.HB4 = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const producto = await Bar.findById(id).lean().exec();
+        const Producto = producto.Producto;
+        const Precio = producto.Precio;
+        const Tipo = producto.Tipo;
+        const Usuario = "Manuel";
+        const Hora =producto.Hora
+        const newUsuario = new HB4({ Producto, Precio, Usuario, Tipo, Hora });
+        await newUsuario.save();
+        await Bar.findByIdAndDelete(id).lean().exec();
+        res.redirect('/Bar');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error interno del servidor");
+    }
+};
+module.exports.HB5 = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const producto = await Bar.findById(id).lean().exec();
+        const Producto = producto.Producto;
+        const Precio = producto.Precio;
+        const Tipo = producto.Tipo;
+        const Usuario = "Manuel";
+        const Hora =producto.Hora
+        const newUsuario = new HB5({ Producto, Precio, Usuario, Tipo, Hora });
+        await newUsuario.save();
+        await Bar.findByIdAndDelete(id).lean().exec();
+        res.redirect('/Bar');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error interno del servidor");
+    }
+};
+module.exports.HB6 = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const producto = await Bar.findById(id).lean().exec();
+        const Producto = producto.Producto;
+        const Precio = producto.Precio;
+        const Tipo = producto.Tipo;
+        const Usuario = "Manuel";
+        const Hora =producto.Hora
+        const newUsuario = new HB6({ Producto, Precio, Usuario, Tipo, Hora });
+        await newUsuario.save();
+        await Bar.findByIdAndDelete(id).lean().exec();
+        res.redirect('/Bar');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error interno del servidor");
+    }
+};
+module.exports.HB7 = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const producto = await Bar.findById(id).lean().exec();
+        const Producto = producto.Producto;
+        const Precio = producto.Precio;
+        const Tipo = producto.Tipo;
+        const Usuario = "Manuel";
+        const Hora =producto.Hora
+        const newUsuario = new HB7({ Producto, Precio, Usuario, Tipo, Hora });
+        await newUsuario.save();
+        await Bar.findByIdAndDelete(id).lean().exec();
+        res.redirect('/Bar');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error interno del servidor");
+    }
+};
+module.exports.HB8 = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const producto = await Bar.findById(id).lean().exec();
+        const Producto = producto.Producto;
+        const Precio = producto.Precio;
+        const Tipo = producto.Tipo;
+        const Usuario = "Manuel";
+        const Hora =producto.Hora
+        const newUsuario = new HB8({ Producto, Precio, Usuario, Tipo, Hora });
+        await newUsuario.save();
+        await Bar.findByIdAndDelete(id).lean().exec();
+        res.redirect('/Bar');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error interno del servidor");
+    }
+};
+module.exports.HB9 = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const producto = await Bar.findById(id).lean().exec();
+        const Producto = producto.Producto;
+        const Precio = producto.Precio;
+        const Tipo = producto.Tipo;
+        const Usuario = "Manuel";
+        const Hora =producto.Hora
+        const newUsuario = new HB9({ Producto, Precio, Usuario, Tipo, Hora });
+        await newUsuario.save();
+        await Bar.findByIdAndDelete(id).lean().exec();
+        res.redirect('/Bar');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error interno del servidor");
+    }
+};
+module.exports.HB10 = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const producto = await Bar.findById(id).lean().exec();
+        const Producto = producto.Producto;
+        const Precio = producto.Precio;
+        const Tipo = producto.Tipo;
+        const Usuario = "Manuel";
+        const Hora =producto.Hora
+        const newUsuario = new HB10({ Producto, Precio, Usuario, Tipo, Hora });
+        await newUsuario.save();
+        await Bar.findByIdAndDelete(id).lean().exec();
+        res.redirect('/Bar');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error interno del servidor");
+    }
+};
+module.exports.HB11 = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const producto = await Bar.findById(id).lean().exec();
+        const Producto = producto.Producto;
+        const Precio = producto.Precio;
+        const Tipo = producto.Tipo;
+        const Usuario = "Manuel";
+        const Hora =producto.Hora
+        const newUsuario = new HB11({ Producto, Precio, Usuario, Tipo, Hora });
+        await newUsuario.save();
+        await Bar.findByIdAndDelete(id).lean().exec();
+        res.redirect('/Bar');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error interno del servidor");
+    }
+};
+module.exports.HB12 = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const producto = await Bar.findById(id).lean().exec();
+        const Producto = producto.Producto;
+        const Precio = producto.Precio;
+        const Tipo = producto.Tipo;
+        const Usuario = "Manuel";
+        const Hora =producto.Hora
+        const newUsuario = new HB12({ Producto, Precio, Usuario, Tipo, Hora });
+        await newUsuario.save();
+        await Bar.findByIdAndDelete(id).lean().exec();
+        res.redirect('/Bar');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error interno del servidor");
+    }
+};
+module.exports.HB13 = async (req, res) => {
+    const id = req.params.id;
+    try {
+        const producto = await Bar.findById(id).lean().exec();
+        const Producto = producto.Producto;
+        const Precio = producto.Precio;
+        const Tipo = producto.Tipo;
+        const Usuario = "Manuel";
+        const Hora =producto.Hora
+        const newUuario = new HB13({ Producto, Precio, Usuario, Tipo, Hora });
+        await newUsuario.save();
+        await Bar.findByIdAndDelete(id).lean().exec();
+        res.redirect('/Bar');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Error interno del servidor");
+    }
+};
+
--- routers/Bar.routes.js
+++ routers/Bar.routes.js
@@ -9,5 +9,28 @@ router.get('/Bar',Bar.mostrar,(req,res)=>{
 router.get('/CuentaHB-1/:id',Bar.HB1)
 //Devolver a la cuenta De HB2
 router.get('/CuentaHB-2/:id',Bar.HB2)
+//Devolver a la cuenta De HB3
+router.get('/CuentaHB-3/:id',Bar.HB3)
+//Devolver a la cuenta De HB4
+router.get('/CuentaHB-4/:id',Bar.HB4)
+//Devolver a la cuenta De HB5
+router.get('/CuentaHB-5/:id',Bar.HB5)
+//Devolver a la cuenta De HB6
+router.get('/CuentaHB-6/:id',Bar.HB6)
+//Devolver a la cuenta De HB7
+router.get('/CuentaHB-7/:id',Bar.HB7)
+//Devolver a la cuenta De HB8
+router.get('/CuentaHB-8/:id',Bar.HB8)
+//Devolver a la cuenta De HB9
+router.get('/CuentaHB-9/:id',Bar.HB9)
+//Devolver a la cuenta De HB10
+router.get('/CuentaHB-10/:id',Bar.HB10)
+//Devolver a la cuenta De HB11
+router.get('/CuentaHB-11/:id',Bar.HB11)
+//Devolver a la cuenta De HB12
+router.get('/CuentaHB-12/:id',Bar.HB12)
+//Devolver a la cuenta De HB13
+router.get('/CuentaHB-13/:id',Bar.HB13)
+
 
 module.exports = router
