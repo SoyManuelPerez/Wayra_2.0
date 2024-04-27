21cf1d6f7e69a0c3e2d89dfa7e4553f8505cc0e7
SoyManuelPerez <Soy_ManuelPerez@outlook.com>
2024-04-27 11:10:38 -0500

    cocina cabañas

--- routers/Cocina.routes.js
+++ routers/Cocina.routes.js
@@ -34,6 +34,29 @@ router.get('/CuentaHB-11/:id',Cocina.HB11)
 router.get('/CuentaHB-12/:id',Cocina.HB12)
 //Devolver a la cuenta De HB13
 router.get('/CuentaHB-13/:id',Cocina.HB13)
-
+//Devolver a la cuenta De CB14
+router.get('/CuentaCB-14/:id',Cocina.CB14)
+//Devolver a la cuenta De CB15
+router.get('/CuentaCB-15/:id',Cocina.CB15)
+//Devolver a la cuenta De CB16
+router.get('/CuentaCB-16/:id',Cocina.CB16)
+//Devolver a la cuenta De CB17
+router.get('/CuentaCB-17/:id',Cocina.CB17)
+//Devolver a la cuenta De CB18
+router.get('/CuentaCB-18/:id',Cocina.CB18)
+//Devolver a la cuenta De CB19
+router.get('/CuentaCB-19/:id',Cocina.CB19)
+//Devolver a la cuenta De CB20
+router.get('/CuentaCB-20/:id',Cocina.CB20)
+//Devolver a la cuenta De CB21
+router.get('/CuentaCB-21/:id',Cocina.CB21)
+//Devolver a la cuenta De CB22
+router.get('/CuentaCB-22/:id',Cocina.CB22)
+//Devolver a la cuenta De CB23
+router.get('/CuentaCB-23/:id',Cocina.CB23)
+//Devolver a la cuenta De CB24
+router.get('/CuentaCB-24/:id',Cocina.CB24)
+//Devolver a la cuenta De CB25
+router.get('/CuentaCB-25/:id',Cocina.CB25)
 
 module.exports = router
