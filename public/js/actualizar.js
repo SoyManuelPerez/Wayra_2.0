60ec1a3e5fc9dfc085af1ad51e951bd8ff3faf69
SoyManuelPerez <Soy_ManuelPerez@outlook.com>
2024-05-06 16:55:18 -0500

    Auto actualizar

---
+++ public/js/actualizar.js
@@ -0,0 +1,7 @@
+ // Función para recargar la página
+ function recargarPagina() {
+    location.reload();
+  }
+
+  // Llamar a la función para recargar cada minuto (60000 milisegundos)
+  setInterval(recargarPagina, 60000)
