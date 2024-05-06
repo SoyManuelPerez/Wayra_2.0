8f7aefd5bb8e3fd5e34e1557725925bdaf28118b
SoyManuelPerez <Soy_ManuelPerez@outlook.com>
2024-05-06 12:55:18 -0500

    MODELOS DE ENTRANTES

---
+++ models/ECB-15.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose')
+const HB = new mongoose.Schema ({
+    Producto: String,
+    Precio:Number,
+    Usuario: String,
+    Tipo: String,
+    Hora: String, 
+})
+module.exports = mongoose.model('ECB15', HB)
