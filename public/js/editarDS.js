73d54310a8421f6de399e4cb6fd952648b4f0c8b
Soy_Manuelperez <Soy_manuelperezutlook.com>
2024-07-06 12:32:30 -0500

    EDITAR DIAS DE SOL

--- public/js/editarDS.js
+++ public/js/editarDS.js
@@ -11,15 +11,10 @@ onl(document,'click','.btnEditarIngreso',e=>{
     const fila = e.target.parentNode.parentNode;
     Comanda.value = fila.children[0].innerHTML
     Nombre.value = fila.children[1].innerHTML
-    Apellido.value = fila.children[2].innerHTML
-    Tipo.value = fila.children[3].innerHTML
-    Documento.value = fila.children[4].innerHTML
-    Abono.value = fila.children[5].innerHTML
-    Pago.value = fila.children[6].innerHTML
-    Adultos.value = fila.children[7].innerHTML
-    Niños.value = fila.children[8].innerHTML
-    Bebes.value = fila.children[9].innerHTML
-    Fecha.value = fila.children[10].innerHTML
+    Adultos.value = fila.children[2].innerHTML
+    Niños.value = fila.children[3].innerHTML
+    Bebes.value = fila.children[4].innerHTML
+    Fecha.value = fila.children[5].innerHTML
     Comanda.setAttribute('readonly', true)
     modalUsuario.show()
 })
